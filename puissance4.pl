21575737b2ce82c88897a55253bbccc4bb55e870
cmc373 <milinaire.c@gmail.com>
2019-10-23 15:06:59 +0200

    correct choose move was added

--- puissance4.pl
+++ puissance4.pl
@@ -7,50 +7,47 @@
 	       ['-','-','-','-','-','-'],
 	       ['-','-','-','-','-','-']]).
 
-:-dynamic(michto/2).
-michto('jeanne', 'frank').
- michto('xxxxjeanne', 'frank').
- michto('jeanneyyyyy', 'frankiii').
-
-
-display:-write(' 1 2 3 4 5 6 7'), nl, draw_table. 
+display:-write(' 1 2 3 4 5 6 7'), nl, draw_table.
 draw_table:-get_position(0).
 
 % Iterate over the number of rows to draw each column
 get_position(6):-nl.
 get_position(N):-draw_column_pos(N,0),NS is  N+1, get_position(NS).
 
-% Draw the pos of each column 
-draw_column_pos(N,7):-nl. 
+% Draw the pos of each column
+draw_column_pos(_,7):-nl.
 draw_column_pos(N,C):-board(X),nth0(C, X, R), nth0(N,R, R1), write(' '), write(R1), CS is C+1, draw_column_pos(N,CS).
-start:-display,play('X').
+start:-reset,display,play('X').
 
-chooseMove('X', N):- \+cannotPlay(N).
-chooseMove('X',N):-cannotPlay(N), write('cannotplay').
+% Ici on va voir si le coup est possible, s'il ne l'est pas on redemande
+chooseMove('X', N, N_ok):- \+cannotPlay(N), N_ok is N.
+chooseMove('X', N, N_ok):- cannotPlay(N),call_column('X', N_ok).
+chooseMove('O', N, N_ok):- \+cannotPlay(N), N_ok is N.
+chooseMove('O', N, N_ok):- cannotPlay(N),call_column('O', N_ok).
 
-askColumn(N):-write(J), write( ':Dans quelle colonne'),read(N).
+cannotPlay(N):- \+integer(N).
+cannotPlay(N):- N>7.
+cannotPlay(N):- N<1.
+cannotPlay(N):- Ns is N-1, board(B),nth0(Ns, B, COL), \+get_free_index_column(COL,6,'s',_).
 
-cannotPlay(N):- \+integer(N);N>7;N<1;Ns is N-1, \+ get_free_index_column(Ns,6,'s',INDEX_LIBRE).
+call_column('X', N_ok) :- write('X: Dans quelle colonne : '), read(N), chooseMove('X', N, N_ok).
+call_column('O', N_ok) :- write('O: Dans quelle colonne : '), read(N), chooseMove('O', N, N_ok).
+%fin du choix de la colonne jouer
 
-
-play('X'):-write('X: Dans quelle colonne'), read(N), Ns is N-1, make_move(Ns,'X'), play('O').
-play('O'):-write('O: Dans quelle colonne'), read(N), Ns is N-1, make_move(Ns, 'O'), play('X').
-
-
-playIa:-random_between(0,6,R)
-
+play('X'):-call_column('X', N_ok), Ns is N_ok-1, make_move(Ns,'X'), play('O').
+play('O'):-call_column('O', N_ok), Ns is N_ok-1, make_move(Ns,'O'), play('X').
 
 % Insert j in nth column with current board b resulting in r
 make_move(N_COL, J):-board(B),nth0(N_COL, B, COL),get_free_index_column(COL,6,'s',INDEX_LIBRE),replace(COL,INDEX_LIBRE, J,COL_RES),replace(B,N_COL, COL_RES, R),retract(board(_)),assert(board(R)),display.
 
 
-% replacing element at index I in L 
+% replacing element at index I in L
 replace([_|T], 0, X, [X|T]).
 replace([H|T], I, X, [H|R]):- I > 0, I1 is I-1, replace(T, I1, X, R).
 
-% finding first index of list which is '-' 
-get_free_index_column(COL, INDEX, '-' , INDEX).
-get_free_index_column(COL, INDEX, VALEUR , INDEX_LIBRE):-INDEX1 is INDEX -1, nth0(INDEX1, COL, VALEUR1), get_free_index_column(COL,INDEX1,VALEUR1,INDEX_LIBRE).  
+% finding first index of list which is '-'
+get_free_index_column(_, INDEX, '-' , INDEX).
+get_free_index_column(COL, INDEX, _, INDEX_LIBRE):-INDEX1 is INDEX -1, nth0(INDEX1, COL, VALEUR1), get_free_index_column(COL,INDEX1,VALEUR1,INDEX_LIBRE).
 
 reset:-retract(board(_)), assert(board([['-','-','-','-','-','-'],
             ['-','-','-','-','-','-'],
