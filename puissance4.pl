70d75820a8ddb9ac5717fa00f6ae0d03e56ad693
corentin laharotte <colapala@gmail.com>
2019-10-31 17:07:36 +0100

    add vertical pieces' count when we want to know the benefices of a move in a specified column

--- puissance4.pl
+++ puissance4.pl
@@ -34,11 +34,11 @@ call_column('X', N_ok) :- write('X: Dans quelle colonne : '), read(N), chooseMov
 call_column('O', N_ok) :- write('O: Dans quelle colonne : '), read(N), chooseMove('O', N, N_ok).
 %fin du choix de la colonne jouer
 
-play('X'):-call_column('X', N_ok), Ns is N_ok-1, make_move(Ns,'X'), play('O').
-play('O'):-call_column('O', N_ok), Ns is N_ok-1, make_move(Ns,'O'), play('X').
+play('X'):-call_column('X', N_ok), Ns is N_ok-1, make_move(Ns,'X',_), play('O').
+play('O'):-call_column('O', N_ok), Ns is N_ok-1, make_move(Ns,'O',_), play('X').
 
 % Insert j in nth column with current board b resulting in r
-make_move(N_COL, J):-board(B),nth0(N_COL, B, COL),get_free_index_column(COL,6,'s',INDEX_LIBRE),replace(COL,INDEX_LIBRE, J,COL_RES),replace(B,N_COL, COL_RES, R),retract(board(_)),assert(board(R)),display.
+make_move(N_COL, J,INDEX_LIBRE):-board(B),nth0(N_COL, B, COL),get_free_index_column(COL,6,'s',INDEX_LIBRE),replace(COL,INDEX_LIBRE, J,COL_RES),replace(B,N_COL, COL_RES, R),retract(board(_)),assert(board(R)),display.
 
 
 % replacing element at index I in L
@@ -46,8 +46,32 @@ replace([_|T], 0, X, [X|T]).
 replace([H|T], I, X, [H|R]):- I > 0, I1 is I-1, replace(T, I1, X, R).
 
 % finding first index of list which is '-'
-get_free_index_column(_, INDEX, '-' , INDEX).
+get_free_index_column(_, INDEX, '-' , INDEX):-!.
 get_free_index_column(COL, INDEX, _, INDEX_LIBRE):-INDEX1 is INDEX -1, nth0(INDEX1, COL, VALEUR1), get_free_index_column(COL,INDEX1,VALEUR1,INDEX_LIBRE).
+
+% Get the number of aligned pieces if we play at the provided column, then come back to the original board
+get_nb_aligned_pieces(NB_COL,Player, Nb_pieces_aligned):- board(Current_board), make_move(NB_COL,Player,INDEX_LIBRE),count_vertical_pieces(Player,NB_COL,INDEX_LIBRE,1, Nb_pieces_aligned),retract(board(_)),assert(board(Current_board)).
+
+% Count the number of similar pieces that are under the last piece that we have put in the column
+	count_vertical_pieces(Player,_,_,4, _). % win
+
+	% Stop when it is the end of the list
+	count_vertical_pieces(Player,_,5,Last_count, Last_count):-!. 
+
+	count_vertical_pieces(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):- board(B), nth0(NB_COL, B, COL),Next_piece is INDEX_LIBRE+1, nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_vertical_pieces(Player,NB_COL,Next_piece,New_Count, Real_Count).
+
+	% Stop when the next piece is different from the current piece
+	count_vertical_pieces(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):- board(B), nth0(NB_COL, B, COL),Next_piece is INDEX_LIBRE+1, \+nth0(Next_piece,COL,Player), Real_Count is Last_count.
+
+% STILL WORKING ON :
+getLine(NB_Line, Line,0):-board(B), nth0(0, B, COL), nth0(NB_Line, COL, Element), append([],Element,Line),getLine(NB_Line, Line,1).
+getLine(NB_Line, Line,Index):-board(B), nth0(Index, B, COL), nth0(NB_Line, COL, Element),Old_line is Line,append(Old_line,Element,Line), Index1 is Index+1,getLine(NB_Line, Line,Index1).
+getLine(NB_Line, Line,6):-!.
+
+count_horizontal_pieces(Player,NB_COL,Last_count, Real_Count).
+count_left_horizontal_pieces(Player,NB_COL,Last_count, Real_Count).
+count_right_horizontal_pieces(Player,NB_COL,Last_count, Real_Count).
+
 
 reset:-retract(board(_)), assert(board([['-','-','-','-','-','-'],
             ['-','-','-','-','-','-'],
