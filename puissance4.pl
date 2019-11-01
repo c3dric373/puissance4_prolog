08a342cadb26060fe97451fda21f971e871f10ac
corentin laharotte <colapala@gmail.com>
2019-11-02 00:29:35 +0100

    add Heur1 but still have to work on the priorities in case of equal scores for a move

--- puissance4.pl
+++ puissance4.pl
@@ -17,7 +17,7 @@ get_position(N):-draw_column_pos(N,0),NS is  N+1, get_position(NS).
 % Draw the pos of each column
 draw_column_pos(_,7):-nl.
 draw_column_pos(N,C):-board(X),nth0(C, X, R), nth0(N,R, R1), write(' '), write(R1), CS is C+1, draw_column_pos(N,CS).
-start:-reset,display,play('X').
+start:-reset,display,play('O').
 
 % Ici on va voir si le coup est possible, s'il ne l'est pas on redemande
 chooseMove('X', N, N_ok):- \+cannotPlay(N), N_ok is N.
@@ -35,7 +35,7 @@ call_column('O', N_ok) :- write('O: Dans quelle colonne : '), read(N), chooseMov
 %fin du choix de la colonne jouer
 
 play('X'):-call_column('X', N_ok), Ns is N_ok-1, make_move(Ns,'X',_),display, play('O').
-play('O'):-call_column('O', N_ok), Ns is N_ok-1, make_move(Ns,'O',_),display, play('X').
+play('O'):-call_column('O', N_ok), Ns is N_ok-1, make_move(Ns,'O',_),display,playIA_heur1('X').%, play('X').
 
 % Insert j in nth column with current board b resulting in r
 make_move(N_COL, J,INDEX_LIBRE):-board(B),nth0(N_COL, B, COL),get_free_index_column(COL,6,'s',INDEX_LIBRE),replace(COL,INDEX_LIBRE, J,COL_RES),replace(B,N_COL, COL_RES, R),retract(board(_)),assert(board(R)).%,display.
@@ -127,14 +127,33 @@ count_diagonal_pieces(Player,NB_COL,INDEX_LIBRE,Count):- count_right_diagonal_pi
 %--------------------------------------------------------------------------------------
 % First Heuristic : Moves that align the more pieces and that disturb the other player
 %--------------------------------------------------------------------------------------
+
 % Give a score to each column : Nb_pieces that we could align MINUS Nb_pieces that the other player could align after our move
 get_list_of_scores_by_column(7, Player,List,List):-!.
 get_list_of_scores_by_column(Nb_col,Player,List, Final_list):- board(Current_board), make_move(Nb_col,Player,INDEX_LIBRE), get_nb_aligned_pieces(Nb_col,Player,INDEX_LIBRE, Nb_pieces_aligned),get_max_score_next_player(0,'O',-5, Max_next_player),Score is Nb_pieces_aligned-Max_next_player,append(List,[Score],New_List),New_Nb_col is Nb_col+1,retract(board(_)), assert(board(Current_board)), get_list_of_scores_by_column(New_Nb_col,Player,New_List, Final_list).
+% If the column is already full we enter -1000 to be sure that it will be never choosen and we continue 
+% Error with this exception get_list_of_scores_by_column(Nb_col,Player,List, Final_list):- \+get_free_index_column(Nb_col,6,'s',_), append(List,[-1000],New_List),New_Nb_col is Nb_col+1,get_list_of_scores_by_column(New_Nb_col,Player,New_List, Final_list). 
 
+% Get the max score that will have the next player, if he plays after us
 get_max_score_next_player(7, Player,Curr_max, Curr_max):-!.
 get_max_score_next_player(Nb_col, Player,Curr_max, Real_max):- board(Current_board), make_move(Nb_col,Player,INDEX_LIBRE), get_nb_aligned_pieces(Nb_col,Player,INDEX_LIBRE, Nb_pieces_aligned), max(Curr_max,Nb_pieces_aligned, New_max),New_Nb_col is Nb_col+1, retract(board(_)), assert(board(Current_board)),get_max_score_next_player(New_Nb_col, Player,New_max, Real_max).
+% If the column is already full we look at the other columns
+% Error with this exception get_max_score_next_player(Nb_col, Player,Curr_max, Real_max):- \+get_free_index_column(Nb_col,6,'s',_),New_Nb_col is Nb_col+1,get_max_score_next_player(New_Nb_col, Player,Curr_max, Real_max).
 
-choose_move_IA_Heur1(Nb_col,Player):-get_list_of_scores_by_column(0,Player,[],List).%choose the max from the List
+max_from_list(List,7,Max,Max):-!.
+max_from_list(List,Index,Max,Real_max):-nth0(Index, List, Value), max(Value,Max, New_max),New_index is Index+1,max_from_list(List,New_index,New_max,Real_max).
+
+get_index_of_max(_,_,List_index,7,List_index):-!.
+get_index_of_max(Max,List_value,List_index,Curr_index,Final_index_list):- nth0(Curr_index, List_value, Max), append(List_index,[Curr_index],New_list), New_index is Curr_index+1,get_index_of_max(Max,List_value,New_list,New_index,Final_index_list).
+get_index_of_max(Max,List_value,List_index,Curr_index,Final_index_list):- \+nth0(Curr_index, List_value, Max),New_index is Curr_index+1,get_index_of_max(Max,List_value,List_index,New_index,Final_index_list).
+
+% Choose the move with the best score
+% if there is just one best move, choose it
+choose_move_IA_Heur1(Nb_col,Player,List,Max,List_index):-get_list_of_scores_by_column(0,Player,[],List),max_from_list(List,0,-8,Max),get_index_of_max(Max,List,[],0,List_index),length(List_index,1),nth0(0, List_index, Nb_col).
+% else choose trandom move among the best ones -- A REVOIR
+choose_move_IA_Heur1(Nb_col,Player,List,Max,List_index):-get_list_of_scores_by_column(0,Player,[],List),max_from_list(List,0,-8,Max),get_index_of_max(Max,List,[],0,List_index), \+length(List_index,1),length(List_index,Size), Index_max is Size-1,random_between(0,Index_max,Nb_col).
+
+playIA_heur1(Player):-choose_move_IA_Heur1(Nb_col,Player,List,Max,List_index),make_move(Nb_col,'X',_),display,play('O').
 
 %----------------- End of First heuristic
 
