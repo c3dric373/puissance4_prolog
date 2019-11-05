03851c728521136bc78baecbffe9265e4c8647bc
cmc373 <milinaire.c@gmail.com>
2019-11-06 00:43:34 +0100

    deleted comments

--- puissance4.pl
+++ puissance4.pl
@@ -187,7 +187,7 @@ playIA_heur1(Player):-choose_move_IA_Heur1(Nb_col,Player,List,Max,List_index),ma
 
 
 get_score_heur3(Board,'X',Score):-get_score_heur3_col(0,Board,'X',0,Res), Score is -Res.
-get_score_heur3(Board,'O',Score):-get_score_heur3_col(0,Board,'O',0,Score).
+get_score_heur3(Board,'O',Score):-get_score_heur3_col(0,Board,'O',0,Score),write('tst').
 
 get_score_heur3_col(0, Board, Player, 0, Res):-Factor is 6,
 nth0(0, Board, COL),
@@ -195,7 +195,9 @@ score_list(COL, Player, 0, SlotsInCol),
 Sum is SlotsInCol * Factor,
 N_Col is 1, 
 get_score_heur3_col(N_Col, Board,Player, Sum,Res).
+
 get_score_heur3_col(6, Board, Player, Sum, Res):-Res is  Sum,!.
+
 get_score_heur3_col(N_Col, Board, Player, Sum, Res):-Factor is 6-N_Col,
  nth0(N_Col, Board, COL),
 score_list(COL, Player, 0, SlotsInCol),
@@ -207,18 +209,6 @@ get_score_heur3_col(Ns_Col, Board,Player, N_Sum,Res).
 
 
 
-
-
-
-/*
-get_score_heur3(Col, Board, Score, Res).
-get_score_column_heur3(Col,1,Score):-sublist(L,1,0), score_list().
-get_score_column_heur3(Col,Index_Row,Player,Score):-Start_Index is Index_Row,
-End_Index is Index_Row + 3,
-sublist(L,Start_Index,End_Index,Col), 
-score_list(L,Player,0,Score).
-get_score_column_heur3(6, Board, Score):-nth0(N_COL, Board, COL),get_free_index_column(COL,6,  's', INDEX_LIBRE).
-*/
 
 % Checks how many slots are equal to Player in List Column
 score_list([], Player, Sum,Res):-Res is Sum.
