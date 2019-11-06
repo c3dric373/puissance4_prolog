0bf7dd6466ba0536e141f5dc3075a8f94ec11a84
corentin laharotte <colapala@gmail.com>
2019-11-06 04:30:58 +0100

    review heur4

--- puissance4.pl
+++ puissance4.pl
@@ -240,68 +240,70 @@ simulate_move(Board, N_COL, J,INDEX_LIBRE,R):-nth0(N_COL, Board, COL),get_free_i
 
 %------------------Heuristic 4 
 
+% get a Line (Needed for the pieces' counting) 
+getLineReview(_,NB_Line, Line,7,Line):-!.
+getLineReview(B,NB_Line, Line,Index,Total_line):-nth0(Index, B, COL), nth0(NB_Line, COL, Element),append(Line,[Element],New_line), Index1 is Index+1,getLine(B,NB_Line, New_line,Index1,Total_line).
+
+
 % Get the number of aligned pieces if we play at the provided column
-get_nb_aligned_piecesReview(NB_COL,Player,INDEX_LIBRE, Nb_pieces_aligned):-count_vertical_piecesReview(Player,NB_COL,INDEX_LIBRE,1, Nb_pieces_vertical),count_horizontal_piecesReview(Player,NB_COL, INDEX_LIBRE,Nb_pieces_horizontal),count_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Nb_pieces_diagonal),max(Nb_pieces_vertical,Nb_pieces_horizontal,Max_vert_hor),max(Max_vert_hor,Nb_pieces_diagonal,Nb_pieces_aligned).
+get_nb_aligned_piecesReview(B,NB_COL,Player,INDEX_LIBRE, Nb_pieces_aligned):-count_vertical_piecesReview(B,Player,NB_COL,INDEX_LIBRE,1, Nb_pieces_vertical),count_horizontal_piecesReview(B,Player,NB_COL, INDEX_LIBRE,Nb_pieces_horizontal),count_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Nb_pieces_diagonal),max(Nb_pieces_vertical,Nb_pieces_horizontal,Max_vert_hor),max(Max_vert_hor,Nb_pieces_diagonal,Nb_pieces_aligned).
 
 % Count the number of similar pieces that are under the last piece that we have put in the column (vertical count)
 
 	% Stop when it is the end of the list
-	count_vertical_piecesReview(Player,_,5,Last_count, Last_count):-!. 
+	count_vertical_piecesReview(B,Player,_,5,Last_count, Last_count):-!. 
 
-	count_vertical_piecesReview(Player,NB_COL,B,INDEX_LIBRE,Last_count, Real_Count):- nth0(NB_COL, B, COL),Next_piece is INDEX_LIBRE+1, nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_vertical_piecesReview(Player,NB_COL,Next_piece,New_Count, Real_Count).
+	count_vertical_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):- nth0(NB_COL, B, COL),Next_piece is INDEX_LIBRE+1, nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_vertical_piecesReview(Player,NB_COL,Next_piece,New_Count, Real_Count).
 
 	% Stop when the next piece is different from the current piece
-	count_vertical_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):- board(B), nth0(NB_COL, B, COL),Next_piece is INDEX_LIBRE+1, \+nth0(Next_piece,COL,Player), Real_Count is Last_count.
+	count_vertical_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):- nth0(NB_COL, B, COL),Next_piece is INDEX_LIBRE+1, \+nth0(Next_piece,COL,Player), Real_Count is Last_count.
 
 % count the number of horizontal aligned pieces when we put a piece in the selected column
 % NB_Line correspond to the INDEX_Libre (index in the column where we have put the piece)
-	count_horizontal_piecesReview(Player,NB_COL, NB_Line,Count):- count_left_horizontal_pieces(Player,NB_COL,NB_Line,1, Count_left),count_right_horizontal_piecesReview(Player,NB_COL,NB_Line,0, Count_right), Count is Count_left+Count_right.
+	count_horizontal_piecesReview(B,Player,NB_COL, NB_Line,Count):- count_left_horizontal_piecesReview(Player,NB_COL,NB_Line,1, Count_left),count_right_horizontal_piecesReview(Player,NB_COL,NB_Line,0, Count_right), Count is Count_left+Count_right.
 
 	% Count the number of similar pieces at the left of the last added piece
-	count_left_horizontal_piecesReview(Player,0,_,Last_count, Last_count):-!.
+	count_left_horizontal_piecesReview(_,_,0,_,Last_count, Last_count):-!.
 	% get the line of the last added piece and compare it to the piece next to it 
-	count_left_horizontal_piecesReview(Player,NB_COL,NB_Line,Last_count, Real_Count):-getLine(NB_Line,[],0,Line), Next_piece is NB_COL-1, nth0(Next_piece,Line,Player),New_Count is Last_count+1,count_left_horizontal_piecesReview(Player,Next_piece,NB_Line,New_Count, Real_Count).
-	count_left_horizontal_piecesReview(Player,NB_COL,NB_Line,Last_count, Real_Count):-getLine(NB_Line,[],0,Line), Next_piece is NB_COL-1, \+nth0(Next_piece,Line,Player),Real_Count is Last_count.
+	count_left_horizontal_piecesReview(B,Player,NB_COL,NB_Line,Last_count, Real_Count):-getLineReview(B,NB_Line,[],0,Line), Next_piece is NB_COL-1, nth0(Next_piece,Line,Player),New_Count is Last_count+1,count_left_horizontal_piecesReview(B,Player,Next_piece,NB_Line,New_Count, Real_Count).
+	count_left_horizontal_piecesReview(B,Player,NB_COL,NB_Line,Last_count, Real_Count):-getLineReview(B,NB_Line,[],0,Line), Next_piece is NB_COL-1, \+nth0(Next_piece,Line,Player),Real_Count is Last_count.
 
 	% Count the number of similar pieces at the right of the last added piece
-	count_right_horizontal_piecesReview(Player,6,NB_Line,Last_count, Last_count):-!.
+	count_right_horizontal_piecesReview(_,_,6,NB_Line,Last_count, Last_count):-!.
 	% get the line of the last added piece and compare it to the piece next to it 
-	count_right_horizontal_piecesReview(Player,NB_COL,NB_Line,Last_count, Real_Count):-getLine(NB_Line,[],0,Line), Next_piece is NB_COL+1, nth0(Next_piece,Line,Player),New_Count is Last_count+1,count_right_horizontal_piecesReview(Player,Next_piece,NB_Line,New_Count, Real_Count).
-	count_right_horizontal_piecesReview(Player,NB_COL,NB_Line,Last_count, Real_Count):-getLine(NB_Line,[],0,Line), Next_piece is NB_COL+1, \+nth0(Next_piece,Line,Player),Real_Count is Last_count.
+	count_right_horizontal_piecesReview(B,Player,NB_COL,NB_Line,Last_count, Real_Count):-getLineReview(B,NB_Line,[],0,Line), Next_piece is NB_COL+1, nth0(Next_piece,Line,Player),New_Count is Last_count+1,count_right_horizontal_piecesReview(B,Player,Next_piece,NB_Line,New_Count, Real_Count).
+	count_right_horizontal_piecesReview(B,Player,NB_COL,NB_Line,Last_count, Real_Count):-getLineReview(B,NB_Line,[],0,Line), Next_piece is NB_COL+1, \+nth0(Next_piece,Line,Player),Real_Count is Last_count.
 
 % count the number of diagonal aligned pieces when we put a piece in the selected column
 % INDEX_LIBRE is the index where we have put the piece
 % Get the max of the right-aligned-diagonal, and the left-aligned-diagonal
-count_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Count):- count_right_diagonal_pieces(Player,NB_COL,INDEX_LIBRE,Right_count), count_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Left_count), max(Right_count,Left_count,Count).
+count_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Count):- count_right_diagonal_pieces(B,Player,NB_COL,INDEX_LIBRE,Right_count), count_left_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Left_count), max(Right_count,Left_count,Count).
 
-	count_right_diagonal_pieces(Player,NB_COL,INDEX_LIBRE,Count):- count_bottom_left_diagonal_pieces(Player,NB_COL,INDEX_LIBRE,1, Left_Count),count_top_right_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,0, Right_Count), Count is Left_Count+Right_Count.
+	count_right_diagonal_pieces(B,Player,NB_COL,INDEX_LIBRE,Count):- count_bottom_left_diagonal_pieces(B,Player,NB_COL,INDEX_LIBRE,1, Left_Count),count_top_right_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,0, Right_Count), Count is Left_Count+Right_Count.
 		% Count the number of piece on the right diagonal under the last put piece
-		count_bottom_left_diagonal_piecesReview(Player,0,_,Last_count, Last_count):-!.
-		count_bottom_left_diagonal_piecesReview(Player,_,5,Last_count, Last_count):-!.
-		count_bottom_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_bottom_left_diagonal_piecesReview(Player,New_NB_COL,Next_piece,New_Count, Real_Count).
-		count_bottom_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
+		count_bottom_left_diagonal_piecesReview(B,Player,0,_,Last_count, Last_count):-!.
+		count_bottom_left_diagonal_piecesReview(B,Player,_,5,Last_count, Last_count):-!.
+		count_bottom_left_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_bottom_left_diagonal_piecesReview(B,Player,New_NB_COL,Next_piece,New_Count, Real_Count).
+		count_bottom_left_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
 
 		% Count the number of piece on the right diagonal above the last put piece
-		count_top_right_diagonal_piecesReview(Player,6,_,Last_count, Last_count):-!.
-		count_top_right_diagonal_piecesReview(Player,_,0,Last_count, Last_count):-!.
-		count_top_right_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_top_right_diagonal_piecesReview(Player,New_NB_COL,Next_piece,New_Count, Real_Count).
-		count_top_right_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
+		count_top_right_diagonal_piecesReview(B,Player,6,_,Last_count, Last_count):-!.
+		count_top_right_diagonal_piecesReview(B,Player,_,0,Last_count, Last_count):-!.
+		count_top_right_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_top_right_diagonal_piecesReview(B,Player,New_NB_COL,Next_piece,New_Count, Real_Count).
+		count_top_right_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
 
-	count_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Count):- count_bottom_right_diagonal_pieces(Player,NB_COL,INDEX_LIBRE,1, Right_Count),count_top_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,0, Left_Count), Count is Left_Count+Right_Count.
+	count_left_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Count):- count_bottom_right_diagonal_pieces(Player,NB_COL,INDEX_LIBRE,1, Right_Count),count_top_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,0, Left_Count), Count is Left_Count+Right_Count.
 		% Count the number of piece on the left diagonal under the last put piece
-		count_bottom_right_diagonal_piecesReview(Player,6,_,Last_count, Last_count):-!.
-		count_bottom_right_diagonal_piecesReview(Player,_,5,Last_count, Last_count):-!.
-		count_bottom_right_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_bottom_right_diagonal_piecesReview(Player,New_NB_COL,Next_piece,New_Count, Real_Count).
-		count_bottom_right_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
+		count_bottom_right_diagonal_piecesReview(B,Player,6,_,Last_count, Last_count):-!.
+		count_bottom_right_diagonal_piecesReview(B,Player,_,5,Last_count, Last_count):-!.
+		count_bottom_right_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_bottom_right_diagonal_piecesReview(B,Player,New_NB_COL,Next_piece,New_Count, Real_Count).
+		count_bottom_right_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL+1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE+1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
 
 		% Count the number of piece on the left diagonal above the last put piece
-		count_top_left_diagonal_piecesReview(Player,0,_,Last_count, Last_count):-!.
-		count_top_left_diagonal_piecesReview(Player,_,0,Last_count, Last_count):-!.
-		count_top_left_diagonal_piecesReview(Player,_,0,Last_count, Last_count):-!.
-		count_top_left_diagonal_piecesReview(Player,_,0,Last_count, Last_count):-!.
-		count_top_left_diagonal_piecesReview(Player,_,0,Last_count, Last_count):-!.
-		count_top_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_top_left_diagonal_piecesReview(Player,New_NB_COL,Next_piece,New_Count, Real_Count).
-		count_top_left_diagonal_piecesReview(Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-board(B), New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
+		count_top_left_diagonal_piecesReview(B,Player,0,_,Last_count, Last_count):-!.
+		count_top_left_diagonal_piecesReview(B,Player,_,0,Last_count, Last_count):-!.
+		count_top_left_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,nth0(Next_piece,COL,Player), New_Count is Last_count+1,count_top_left_diagonal_piecesReview(B,Player,New_NB_COL,Next_piece,New_Count, Real_Count).
+		count_top_left_diagonal_piecesReview(B,Player,NB_COL,INDEX_LIBRE,Last_count, Real_Count):-New_NB_COL is NB_COL-1,nth0(New_NB_COL, B, COL), Next_piece is INDEX_LIBRE-1,\+nth0(Next_piece,COL,Player),Real_Count is Last_count.
 
 
 get_score_heur4(Nb_col,Board, Score):-get_nb_aligned_pieces(NB_COL,Player,INDEX_LIBRE, Nb_pieces_aligned)
